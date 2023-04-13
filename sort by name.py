f2b49403aefb5056fc5c40c9df2eb6423e784541
amaresh-git-dev <amaresh1rout@gmail.com>
2023-04-13 17:15:40 +0530

    1st try successful

---
+++ sort by name.py
@@ -0,0 +1,11 @@
+#print("Enter your names without using any type of special!!!")
+
+name1 = input("Your Names Please: ")
+
+#spacing = name1.split()
+
+name1.sort()
+print(name1)
+for i in name1:
+    print(i)
+    
