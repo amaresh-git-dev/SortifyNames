ee3e3ce9a20a4b2801827ec2bcd08fc88c2e884e
Amaresh Rout <60665946+amaresh-git-dev@users.noreply.github.com>
2023-04-29 16:24:12 +0530

    Update sort by name.py

--- sort by name.py
+++ sort by name.py
@@ -1,11 +1,5 @@
-#print("Enter your names without using any type of special!!!")
-
 name1 = input("Your Names Please: ").split(',')
-
-#spacing = name1.split()
-
 name1.sort()
-
 for i in name1:
     print(i)
     
